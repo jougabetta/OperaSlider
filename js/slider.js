dd0d7ee78edf4e6752594233b55c7a0e45a1d537
Jonathan <jougabetta@gmail.com>
2013-03-12 14:49:28 -0300

    adicionado opcao de 360

--- js/slider.js
+++ js/slider.js
@@ -5,11 +5,13 @@ $.fn.sliderOpera = function(settings){
         visible: 1,
         pass: 1,
         fade: false,
-        speed: 900
+        speed: 900,
+        end: 1
 
    }
 
    if(settings){ $.extend( o, settings); }
+
    $size=0;
    $slider = this;
 
@@ -35,55 +37,155 @@ $.fn.sliderOpera = function(settings){
 
    }).get();
 
-   $slider.css({ width: $widthLi*o.visible+"px", height: $heightLi*o.visible+"px" }).find("ul").css({width: $size+"px", position: "relative", margin: 0, padding: 0});
+   $slider.css({ width: $widthLi*o.visible+"px", height: $heightLi }).find("ul").css({width: $size+"px", position: "relative", margin: 0, padding: 0});
 
-   $totalArrayOffset = o.pass == 1 && o.visible != 1 ? parseInt($arrayOffset.length / o.visible + o.pass) : $arrayOffset.length / o.pass
+   if(o.pass != 1 && o.visible != 1 ){
 
-   $slider.append(function(){
+      $totalArrayOffset = Math.ceil($arrayOffset.length / o.visible);
 
-        $texto = "<div class='painel_button'>";
-        for($i=0;$i < $totalArrayOffset; $i++){
-            $texto += "<div class='button'></div>";
-        }
-        $texto += "</div>";
+   }else{
 
-        return $texto;
+      $totalArrayOffset = $arrayOffset.length / o.pass;
 
-   });
+   }
 
-   $slider.delegate(".button", "click", function(){
+   if( o.end ){
 
-        $i = $(this).index();
+      withEnd();
 
-        if(o.fade){
+   }else{
 
-            $slider.find("li").stop();
-            $slider.find("li").css({zIndex: 999}).eq($i).css({zIndex: 1000}).animate({
+      noEnd();
 
-                opacity: 1
+   }
 
-            }, o.speed, function(){ $(this).siblings().css({opacity: 0}); });
+   function noEnd(){
+
+      $slider.find("ul").css({left: -$arrayOffset[1].position().left});
+
+      $slider.after('<div class="nextOpera"></div>').before('<div class="prevOpera"></div>');
+      
+      renewArray();
+
+      $(document).delegate(".prevOpera", "click", function(){
+
+          $slider.find("ul").stop();
+          $slider.find("ul").animate({
+
+            left: -$arrayOffset[0].position().left*o.pass+"px"  
+
+          }, function(){
+
+            $slider.find("ul").stop();
+            $slider.find("ul").css({left: -$arrayOffset[1].position().left});
+            renewArray();
+
+          });
+
+      });
+
+      $(document).delegate(".nextOpera", "click", function(){
+
+          $slider.find("ul").stop();
+          $slider.find("ul").animate({
+
+            left: -$arrayOffset[2].position().left*o.pass+"px"  
+
+          }, function(){
+
+            $slider.find("ul").stop();
+            $slider.find("ul").css({left: -$arrayOffset[1].position().left});
+            renewArray(true);
+
+          });
+
+      });
+
+   }
+
+   function renewArray(next){
+
+        if(next){
+
+            $slider.find("ul").append(function(){
+
+            $copy = $arrayOffset[0].clone();
+            $arrayOffset[0].remove();
+
+            $arrayOffset.shift();
+            $arrayOffset.push($copy);
+
+            return $copy;
+
+            });
 
         }else{
 
-            $slider.find("ul").stop();
-            $slider.find("ul").animate({
+            $slider.find("ul").prepend(function(){
 
-                left: -$arrayOffset[$i].position().left*o.pass+"px"
+            $copy = $arrayOffset[$arrayOffset.length-1].clone();
+            $arrayOffset[$arrayOffset.length-1].remove();
+
+            $arrayOffset.pop();
+            $arrayOffset.unshift($copy);
+
+            return $copy;
 
             });
 
         }
 
+   }
+   
+   function withEnd(){
+
+       $slider.append(function(){
+
+            $texto = "<div class='painel_button'>";
+            for($i=0;$i < $totalArrayOffset; $i++){
+                $texto += "<div class='button'></div>";
+            }
+            $texto += "</div>";
+
+            return $texto;
+
+       });
+
+       $slider.delegate(".button", "click", function(){
+
+            $i = $(this).index();
+
+            if(o.fade){
+
+                $slider.find("li").stop();
+                $slider.find("li").css({zIndex: 999}).eq($i).css({zIndex: 1000}).animate({
+
+                    opacity: 1
+
+                }, o.speed, function(){ $(this).siblings().css({opacity: 0}); });
+
+            }else{
+
+                $slider.find("ul").stop();
+                $slider.find("ul").animate({
+
+                    left: -$arrayOffset[$i].position().left*o.pass+"px"
+
+                });
+
+            }
 
 
-   });
+
+       });
+
+   }
 
 }
 
 $(function(){
 
-    $(document).find(".slider").sliderOpera({fade: true});
+    $(document).find(".slider").sliderOpera({end:0, visible:3});
 
 });
 
